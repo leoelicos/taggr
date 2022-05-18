8d1af8fdde59c3bf6f0fb4d36ff9f9fba46244c2
Leo Wong <leoelicos@gmail.com>
2022-05-18 13:37:37 +1000

    Associations: Category, Product, Tag, ProductTag

--- models/index.js
+++ models/index.js
@@ -6,15 +6,36 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
 
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
 // Tags belongToMany Products (through ProductTag)
 
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
