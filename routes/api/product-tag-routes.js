b993f7cacefbecdb704050ea081018c10d03eb27
Leo Wong <leoelicos@gmail.com>
2022-05-20 19:58:47 +1000

    Ask Sequelize to order Product Tags by snake case

--- routes/api/product-tag-routes.js
+++ routes/api/product-tag-routes.js
@@ -1,15 +1,10 @@
 const router = require('express').Router();
 const { ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all product tags
 router.get('/', async (req, res) => {
-  // find all product tags
-
   try {
     const productTagData = await ProductTag.findAll({
-      order: [['tagId', 'ASC']]
+      order: [['tag_id', 'ASC']]
     });
     console.log(JSON.stringify(productTagData));
     res.status(200).json(productTagData);
